a5e7e1e3a24549ee694a60fea4c70137a96062ac
Henry Kim <heyhenrykim@gmail.com>
2023-03-07 16:12:09 -0500

    Something I saw on YouTube

---
+++ namedictionary.py
@@ -0,0 +1,13 @@
+names = [
+    'Teresa'
+    'Henry'
+    'Jaden'
+]
+
+# List Comprehension
+#length = [len(name) for name in names]
+
+# Dictionary Comprehension
+length = {name:len(name) for name in names} 
+print(length)
+
